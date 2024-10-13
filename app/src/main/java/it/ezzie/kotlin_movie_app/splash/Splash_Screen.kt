764caebf232dcc71a9731f13b5160be658decaeb
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-13 21:32:19 +0700

    Initial Commit

---
+++ app/src/main/java/it/ezzie/kotlin_movie_app/splash/Splash_Screen.kt
@@ -0,0 +1,17 @@
+package it.ezzie.kotlin_movie_app.splash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import it.ezzie.kotlin_movie_app.R
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
