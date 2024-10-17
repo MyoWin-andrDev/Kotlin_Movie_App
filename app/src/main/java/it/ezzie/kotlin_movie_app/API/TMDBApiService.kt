f72d593725ea204d43a78883f0b8f8ab5432ecd4
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-17 23:26:53 +0700

    TMDBApiService.kt functions creating

--- app/src/main/java/it/ezzie/kotlin_movie_app/API/TMDBApiService.kt
+++ app/src/main/java/it/ezzie/kotlin_movie_app/API/TMDBApiService.kt
@@ -2,12 +2,15 @@ package it.ezzie.kotlin_movie_app.API
 
 import androidx.browser.trusted.Token
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface TMDBApiService {
 
     fun getPopularMovie(
 
         @Header("Authorization") authToken : String,
-        @Header("accept") accept : String = "application/json"
+        @Header("accept") accept : String = "application/json",
+        @Query("language") language : String ="en-US",
+        @Query("page") page: Int = 1
     )
 }
