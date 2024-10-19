5ce1117b6beda45ae3cc0b8ee78777a59909799d
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-20 01:46:05 +0700

    Creating API Service in HomePage.kt

--- app/src/main/java/it/ezzie/kotlin_movie_app/view/HomePage.kt
+++ app/src/main/java/it/ezzie/kotlin_movie_app/view/HomePage.kt
@@ -7,6 +7,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import it.ezzie.kotlin_movie_app.API.TMDBApiService
 import it.ezzie.kotlin_movie_app.R
+import it.ezzie.kotlin_movie_app.data.Movie
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -30,5 +34,16 @@ class HomePage : AppCompatActivity() {
         val authToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzcxNDkwMjY5MjJiZmQ0YTY4MmYyZWFiYTNkOGFiZiIsIm5iZiI6MTcyOTM1ODAyMy4zMTkzMjksInN1YiI6IjY3MTNlNzU3OTlmMjJmMzI2YWFkMjJhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EcdIVTEeNaUcyOtV_cOpr8oaO5go6K2KklKXdQL_NR0"
 
         //Making Api Call
+        val call = apiService.getPopularMovie(authToken)
+        call.enqueue(object:Callback<Movie>{
+            override fun onResponse(p0: Call<Movie>, p1: Response<Movie>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(p0: Call<Movie>, p1: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
