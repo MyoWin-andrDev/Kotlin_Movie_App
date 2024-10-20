d09b6e68403121eaaccbdff863ae47088f8a7e84
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-20 12:35:39 +0700

    Init movie UI

--- app/src/main/java/it/ezzie/kotlin_movie_app/adapter/PopularAdapter.kt
+++ app/src/main/java/it/ezzie/kotlin_movie_app/adapter/PopularAdapter.kt
@@ -1,4 +1,27 @@
 package it.ezzie.kotlin_movie_app.adapter
 
-class PopularAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import it.ezzie.kotlin_movie_app.data.Movie
+import it.ezzie.kotlin_movie_app.databinding.PopularMovieAdapterBinding
+
+class PopularAdapter (val context: Context, val movie : Movie) : RecyclerView.Adapter<PopularAdapter.PopularViewHolder>(){
+
+    class PopularViewHolder (private var binding : PopularMovieAdapterBinding) : RecyclerView.ViewHolder(binding.root){}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+        val layoutInflater : LayoutInflater = LayoutInflater.from(parent.context)
+        val binding = PopularMovieAdapterBinding.inflate(layoutInflater)
+        return PopularViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+
+    }
 }
